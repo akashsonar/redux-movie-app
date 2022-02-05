89a84af0b2e7c9ebe8e4aaab1499a98235e58e3d
akash sonar <sonarakash888@gmail.com>
2022-02-05 09:38:54 +0530

    js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home.jsx";
+import Home from "./components/Home/Home.js";
 import MovieDetail from "./components/MovieDetail/MovieDetail";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 
